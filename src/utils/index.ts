e747120c02bb1c47e1992a1f125f871fc70c9381
somostodoskayky <somostodoskayky@gmail.com>
2025-10-21 04:44:15 -0700

    update utils/index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const YOUTUBE_API_KEY = process.env.VITE_YOUTUBE_API_KEY;
-export const GOOGLE_CLIENT_ID = process.env.VITE_GOOGLE_CLIENT_ID;
-export const GOOGLE_CLIENT_SECRET = process.env.VITE_GOOGLE_CLIENT_SECRET;
+export const YOUTUBE_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+export const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = import.meta.env.VITE_GOOGLE_CLIENT_SECRET;
 export const GOOGLE_REDIRECT_URI = `${window.location.origin}/auth/callback`;
